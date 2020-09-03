c045d0155a6c771a27027b14326cc4463506370c
GanatraJay2000 <jbg@somaiya.edu>
2020-09-03 09:29:38 +0530

    initial commit

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@ if ($selecting->num_rows > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHPPDF</title>
+    <title>PHPEXCEL</title>
     <link rel="stylesheet" href="./my_vendors/bootstrap.min.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
